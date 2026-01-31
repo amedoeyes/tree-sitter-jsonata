a99bc4035f92b3fa510ca5f84dab58acd4409458
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2026-02-01 01:29:56 +0200

    feat: implement rest of the language

--- grammar.js
+++ grammar.js
@@ -14,16 +14,176 @@ module.exports = grammar({
 
 	rules: {
 		program: ($) => $.expression,
-		expression: ($) => choice($.bind_expr, $.call_expr),
-		call_expr: ($) => seq($.bind_expr, "(", optional($.argument_list), ")"),
-		argument_list: ($) => seq($.expression, repeat(seq(",", $.expression))),
-		bind_expr: (_) =>
+
+		comment: (_) => token(seq("/*", repeat(/[^/]/), "*/")),
+
+		expression: ($) =>
 			choice(
-				token("$"),
-				token("$$"),
-				token("%"),
-				seq("$", /[a-zA-Z_][a-zA-Z0-9_]*/),
+				$.literal,
+				$.function_expr,
+				$.call_expr,
+				$.access_expr,
+				$.wildcard_expr,
+				$.bind_expr,
+				$.condition_expr,
+				$.transform_expr,
+				$.binary_expr,
+				$.parent_expr,
+				$.filter_expr,
+				$.order_expr,
+				$.reduce_expr,
+				$.negate_expr,
+				$.paren_expr,
 			),
-		comment: (_) => token(seq("/*", /[^*]*\*+([^/*][^*]*\*+)*/, "/")),
+
+		function_expr: ($) =>
+			seq("function", "(", sepBy(",", $.bind), ")", "{", $.expression, "}"),
+
+		call_expr: ($) =>
+			seq(
+				field("function", choice($.identifier, $.bind)),
+				"(",
+				sepBy(",", field("argument", choice($.expression, "?"))),
+				")",
+			),
+
+		wildcard_expr: (_) => prec(10, choice("*", "**")),
+
+		bind_expr: ($) => prec.right(1, seq($.bind, ":=", $.expression)),
+
+		filter_expr: ($) => prec(8, seq($.expression, "[", $.expression, "]")),
+
+		order_expr: ($) =>
+			prec(
+				4,
+				seq(
+					$.expression,
+					"^",
+					"(",
+					choice(
+						$.expression,
+						sepBy(",", seq(choice("<", ">"), $.access_expr)),
+					),
+					")",
+				),
+			),
+
+		reduce_expr: ($) => prec(7, seq($.expression, $.object)),
+
+		transform_expr: ($) =>
+			prec(
+				2,
+				seq(
+					"|",
+					$.expression,
+					"|",
+					$.expression,
+					optional(seq(",", $.expression)),
+					"|",
+				),
+			),
+
+		parent_expr: (_) => "%",
+
+		condition_expr: ($) =>
+			prec.right(
+				1,
+				seq($.expression, "?", $.expression, optional(seq(":", $.expression))),
+			),
+
+		binary_expr: ($) => {
+			const operators = [
+				[2, "or"],
+				[3, "and"],
+				[4, "??"],
+				[4, "?:"],
+				[4, ".."],
+				[5, "in"],
+				[5, "="],
+				[5, "!="],
+				[5, "<"],
+				[5, ">"],
+				[5, "<="],
+				[5, ">="],
+				[5, "~>"],
+				[6, "&"],
+				[6, "+"],
+				[6, "-"],
+				[7, "*"],
+				[7, "/"],
+				[7, "%"],
+				[8, "."],
+				[9, "#"],
+				[9, "@"],
+			];
+			return choice(
+				...operators.map(([bp, op]) =>
+					prec.left(
+						bp,
+						seq(field("left", $.expression), op, field("right", $.expression)),
+					),
+				),
+			);
+		},
+
+		negate_expr: ($) => prec.right(7, seq("-", field("right", $.expression))),
+
+		access_expr: ($) => choice($.bind, $.identifier),
+
+		identifier: (_) => /[A-Za-z_][A-Za-z0-9_'"]*/,
+
+		bind: (_) => choice("$", "$$", seq("$", /['"]?[A-Za-z_][A-Za-z0-9_'"]*/)),
+
+		paren_expr: ($) => seq("(", sepBy(";", $.expression), optional(";"), ")"),
+
+		literal: ($) =>
+			choice($.number, $.string, $.regex, $.boolean, $.array, $.object, $.null),
+
+		number: (_) => /(0|[1-9][0-9]*)(\.[0-9]+)?([Ee][-+]?[0-9]+)?/,
+
+		string: (_) =>
+			token(
+				choice(
+					seq('"', repeat(choice(/[^"\\]/, /\\./)), '"'),
+					seq("'", repeat(choice(/[^'\\]/, /\\./)), "'"),
+				),
+			),
+
+		regex: (_) => token(seq("/", /([^\\/]|\\.)+/, "/", /[im]*/)),
+
+		boolean: (_) => choice("true", "false"),
+
+		array: ($) => seq("[", sepBy(",", $.expression), optional(","), "]"),
+
+		object: ($) => seq("{", sepBy(",", $.pair), "}"),
+
+		pair: ($) =>
+			seq(field("key", $.expression), ":", field("value", $.expression)),
+
+		null: (_) => "null",
 	},
 });
+
+/**
+ * Creates a rule to match one or more of the rules separated by the separator.
+ *
+ * @param {RuleOrLiteral} sep - The separator to use.
+ * @param {RuleOrLiteral} rule
+ *
+ * @returns {SeqRule}
+ */
+function sepBy1(sep, rule) {
+	return seq(rule, repeat(seq(sep, rule)));
+}
+
+/**
+ * Creates a rule to optionally match one or more of the rules separated by the separator.
+ *
+ * @param {RuleOrLiteral} sep - The separator to use.
+ * @param {RuleOrLiteral} rule
+ *
+ * @returns {ChoiceRule}
+ */
+function sepBy(sep, rule) {
+	return optional(sepBy1(sep, rule));
+}
--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,3 +1,75 @@
 (comment) @comment
-(bind_expr) @variable
-(call_expr) @function
+
+(identifier) @variable
+(bind) @variable
+
+(call_expr (bind) @function)
+(call_expr argument: "?" @variable)
+
+(binary_expr
+  "~>"
+  (expression (access_expr (bind) @function)))
+
+(bind_expr
+  (bind) @function
+  (expression (function_expr)))
+
+(bind_expr
+  (bind) @function
+  (expression (call_expr argument: "?")))
+
+(null) @constant.builtin
+(boolean) @constant.builtin.boolean
+(number) @constant.builtin.numeric
+(string) @string
+(regex) @string.regex
+
+"function" @keyword
+
+"(" @punctuation.bracket
+")" @punctuation.bracket
+"[" @punctuation.bracket
+"]" @punctuation.bracket
+"{" @punctuation.bracket
+"}" @punctuation.bracket
+":" @punctuation.delimiter
+"," @punctuation.delimiter
+";" @punctuation.delimiter
+
+"or" @keyword.operator
+"and" @keyword.operator
+"in" @keyword.operator
+"=" @operator
+"!=" @operator
+"<" @operator
+">" @operator
+"<=" @operator
+">=" @operator
+"+" @operator
+"-" @operator
+"*" @operator
+"**" @operator
+"/" @operator
+"%" @operator
+".." @operator
+"~>" @operator
+"&" @operator
+"??" @operator
+"?:" @operator
+":=" @operator
+"." @operator
+"^" @operator
+"#" @operator
+"@" @operator
+
+(condition_expr
+  "?" @operator
+  ":" @operator)
+
+(transform_expr "|" @operator)
+(filter_expr  "[" @operator)
+(filter_expr  "]" @operator)
+(reduce_expr (object "{" @operator))
+(reduce_expr (object "}" @operator))
+(order_expr "(" @operator)
+(order_expr ")" @operator)
